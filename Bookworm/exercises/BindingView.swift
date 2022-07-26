ea65ac35801e05b0fb8f5d18588b61d493b60e6e
Beto Toro <betotoro0902@gmail.com>
2022-07-26 16:06:02 -0500

    [feat]: @Binding

---
+++ Bookworm/exercises/BindingView.swift
@@ -0,0 +1,44 @@
+//
+//  BindingView.swift
+//  Bookworm
+//
+//  Created by Beto Toro on 26/07/22.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+  let title: String
+  @Binding var isOn: Bool
+  
+  var onColors = [Color.red, Color.yellow]
+  var offColors = [Color(white: 0.6), Color(white: 0.4)]
+  
+  var body: some View {
+    Button(title) {
+      isOn.toggle()
+    }
+    .padding()
+    .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+    .foregroundColor(.white)
+    .clipShape(Capsule())
+    .shadow(radius: isOn ? 0 : 5)
+  }
+}
+
+struct BindingView: View {
+  @State private var rememberMe = false
+  
+  var body: some View {
+    VStack {
+      PushButton(title: "Remember Me", isOn: $rememberMe)
+      Text(rememberMe ? "On" : "Off")
+    }
+  }
+}
+
+struct BindingView_Previews: PreviewProvider {
+  static var previews: some View {
+    BindingView()
+  }
+}
