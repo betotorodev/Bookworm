a9c65a1fedbd165e5642bf4358aab2a4a97c6455
Beto Toro <betotoro0902@gmail.com>
2022-07-27 16:56:55 -0500

    [feat]: create a book detail view and sorting method

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -10,26 +10,47 @@ import SwiftUI
 struct ContentView: View {
   
   @Environment(\.managedObjectContext) var moc
-  @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+  @FetchRequest(sortDescriptors: [
+    SortDescriptor(\.title),
+    SortDescriptor(\.author)
+  ]) var books: FetchedResults<Book>
   
   @State private var showingAddScreen = false
   
   var body: some View {
     NavigationView {
-      Text("Count: \(books.count)")
-        .navigationTitle("Bookworm")
-        .toolbar {
-          ToolbarItem(placement: .navigationBarTrailing) {
-            Button {
-              showingAddScreen.toggle()
-            } label: {
-              Label("Add Book", systemImage: "plus")
+      List {
+        ForEach(books) { book in
+          NavigationLink {
+            DetailView(book: book)
+          } label: {
+            HStack {
+              EmojiRatingView(rating: book.rating)
+                .font(.largeTitle)
+              
+              VStack(alignment: .leading) {
+                Text(book.title ?? "Unknown Title")
+                  .font(.headline)
+                Text(book.author ?? "Unknown Author")
+                  .foregroundColor(.secondary)
+              }
             }
           }
         }
-        .sheet(isPresented: $showingAddScreen) {
-          AddBookView()
+      }
+      .navigationTitle("Bookworm")
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          Button {
+            showingAddScreen.toggle()
+          } label: {
+            Label("Add Book", systemImage: "plus")
+          }
         }
+      }
+      .sheet(isPresented: $showingAddScreen) {
+        AddBookView()
+      }
     }
   }
 }
